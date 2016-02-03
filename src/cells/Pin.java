bc1234aaaf71d6096adea86910f95d64b31f9033
Alexander Pliskin <alexnetmore@gmail.com>
2016-02-03 17:00:44 +0300

    Cells

--- src/cells/Pin.java
+++ src/cells/Pin.java
@@ -1,11 +1,33 @@
 package cells;
 
-/**
- * Created by aleksandrpliskin on 03.02.16.
- */
 public class Pin implements Cell {
+
+    double x;
+    double y;
+
+    long id;
+    float name;
+
+    public Pin(double x, double y, long id, float name) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        this.name = name;
+    }
+
+
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    @Override
+    public double getY() {
+        return y;
     }
 }
